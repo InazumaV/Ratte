243fa7435a78617a8bcfae1d4458e277cbc77cb4
Yuzuki616 <yuzuki2332@gmail.com>
2025-05-24 18:49:56 +0900

    fix:

--- common/number/number.go
+++ common/number/number.go
@@ -2,8 +2,8 @@ package number
 
 type Number interface {
 	~int | ~int8 | ~int16 | ~int32 | ~int64 |
-		~float32 | ~float64 |
-		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64
+	~float32 | ~float64 |
+	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64
 }
 
 func SelectBigger[T Number](num1 T, num2 T) T {
@@ -24,5 +24,5 @@ func SelectNotZero[T Number](num1 T, num2 T) T {
 	if num1 != 0 {
 		return num1
 	}
-	return num1
+	return num2
 }
